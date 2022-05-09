81be63cef2281ef41ae269705fb192b6fb72b15f
sysfel <felipe@climberhotel.com>
2022-05-09 07:48:54 +0100

    refresh tab titles and content

--- lib/service/hotel.service.dart
+++ lib/service/hotel.service.dart
@@ -1,12 +1,13 @@
 import 'dart:convert';
 
+import 'package:climber_monitoring/main.dart';
 import 'package:climber_monitoring/model/hotel.dart';
 import 'package:flutter/material.dart';
 
 class HotelService {
 
   static const String response = '[{ "hotelId": 1001160, "hotelName": "BT Garden Bauru", "hotelCode": "BRBAUBGB", "integrationSLA": "KpisCalculatedTime", "lastUpdate": "2022-04-23T01:47:01.000+0000", "integrationStatus": 2, "hpId": 1000000}, { "hotelId": 100116099, "hotelName": "Blue Balls", "hotelCode": "XXXXNNNN", "integrationSLA": "RatesCalculatedTime", "lastUpdate": "2022-04-23T01:47:01.000+0000", "integrationStatus": 1, "hpId": 1000000}, { "hotelId": 100999099, "hotelName": "KPIS BLUE BALLS", "hotelCode": "BCUOUAAAA", "integrationSLA": "KpisCalculatedTime", "lastUpdate": "2022-04-01T01:47:01.000+0000", "integrationStatus": 1, "hpId": 1000000}]';
-  static const String response2 = '[{ "hotelId": 1001160, "hotelName": "BT Garden Bauru", "hotelCode": "BRBAUBGB", "integrationSLA": "KpisCalculatedTime", "lastUpdate": "2022-04-23T01:47:01.000+0000", "integrationStatus": 2, "hpId": 1000000}, { "hotelId": 100116099, "hotelName": "Blue Balls", "hotelCode": "XXXXNNNN", "integrationSLA": "RatesCalculatedTime", "lastUpdate": "2022-04-23T01:47:01.000+0000", "integrationStatus": 1, "hpId": 1000000}, { "hotelId": 100999099, "hotelName": "KPIS BLUE BALLS", "hotelCode": "BCUOUAAAA", "integrationSLA": "KpisCalculatedTime", "lastUpdate": "2022-04-01T01:47:01.000+0000", "integrationStatus": 1, "hpId": 1000000} , { "hotelId": 100999099, "hotelName": "KPIS BLUE BALLS", "hotelCode": "BCUOUAAAA", "integrationSLA": "KpisCalculatedTime", "lastUpdate": "2022-04-01T01:47:01.000+0000", "integrationStatus": 1, "hpId": 1000000}]';
+  static const String response2 = '[{ "hotelId": 1001160, "hotelName": "BT Garden Bauru", "hotelCode": "BRBAUBGB", "integrationSLA": "KpisCalculatedTime", "lastUpdate": "2022-04-23T01:47:01.000+0000", "integrationStatus": 2, "hpId": 1000000}, { "hotelId": 100116099, "hotelName": "Blue Balls", "hotelCode": "XXXXNNNN", "integrationSLA": "RatesCalculatedTime", "lastUpdate": "2022-04-23T01:47:01.000+0000", "integrationStatus": 1, "hpId": 1000000}, { "hotelId": 100999099, "hotelName": "KPIS BLUE BALLS", "hotelCode": "BCUOUAAAA", "integrationSLA": "KpisCalculatedTime", "lastUpdate": "2022-04-01T01:47:01.000+0000", "integrationStatus": 1, "hpId": 1000000} , { "hotelId": 100999099, "hotelName": "KPIS BLUE BALLS 2", "hotelCode": "BCUOUAAAA2", "integrationSLA": "KpisCalculatedTime", "lastUpdate": "2022-04-01T01:47:01.000+0000", "integrationStatus": 1, "hpId": 1000000}, { "hotelId": 100116099, "hotelName": "Blue Balls", "hotelCode": "XXXXNNNN2", "integrationSLA": "RatesCalculatedTime", "lastUpdate": "2022-04-23T01:47:01.000+0000", "integrationStatus": 1, "hpId": 1000000}]';
 
   List<Hotel> fetchHotels() {
     return parseHotels(response);
@@ -35,86 +36,91 @@ class HotelService {
 
 //-----Widgets-----
 
+class HotelPage extends StatefulWidget {
+  final String title;  
+  const HotelPage({ Key? key, required this.title }) : super(key: key);
+
+  @override
+  State<HotelPage> createState() => _HotelPageState(this.title);
+}
+
+class _HotelPageState extends State<HotelPage> {
+
+  _HotelPageState(this.title);
+
+  final String title;
+  List<Hotel> hotels = hotelService.fetchHotels();
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(      
+      appBar: AppBar(
+        title: Text(title),
+        backgroundColor: const Color.fromRGBO(25, 192, 255, 1),
+        bottom: TabBar(tabs: [
+          Tab(text: onTab(HotelIntegrationType.KpisCalculatedTime)),
+          Tab(text: onTab(HotelIntegrationType.RatesCalculatedTime)),
+        ],)
+      ),
+      body: TabBarView(children: hotelService.buildHotels(hotels),),      
+      floatingActionButton: FloatingActionButton(
+        onPressed: refresh,
+        backgroundColor: const Color.fromRGBO(25, 192, 255, 1),
+        child: const Icon(Icons.update),
+        
+      ),
+    );
+  }
+  
+  void refresh() {
+    setState(() {
+      hotels = hotelService.fetchHotels1();
+    });
+  }
+
+  String onTab(HotelIntegrationType type){
+    switch(type) {
+      case HotelIntegrationType.KpisCalculatedTime: 
+        return 'PMS (' + hotelService.countByIntegration(hotels, type).toString() + ')';
+      case HotelIntegrationType.RatesCalculatedTime:
+        return 'RS (' + hotelService.countByIntegration(hotels, type).toString() + ')';
+      default: {
+        return 'Others' + hotelService.countByIntegration(hotels, type).toString() + ')';
+      }  
+    }
+  }
+}
+
 FutureBuilder<List<Hotel>> buildHotelWidget(Future<List<Hotel>> listHotels){
   return FutureBuilder<List<Hotel>>(
       future: listHotels,      
       builder: (context, snapshot) {
-        if(snapshot.hasError) {
-          debugPrint(snapshot.error.toString());
-          return const Center(
-            child: Text('Unexpected error 1'),
-          );
-        } else if (snapshot.hasData) {
-          return HotelList(hotels: snapshot.data!);
-        } else {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        }
+        if(snapshot.hasError) { debugPrint(snapshot.error.toString()); return const Center(child: Text('Unexpected error'),); } 
+        else if (snapshot.hasData) { return HotelList(hotels: snapshot.data!); } 
+        else { return const Center(child: CircularProgressIndicator(),);}
       }
   );
 }
 
-class HotelList extends StatefulWidget {
+class HotelList extends StatelessWidget {
+  const HotelList({ Key? key, required this.hotels }) : super(key: key);
 
   final List<Hotel> hotels;
-  const HotelList({ Key? key, required this.hotels  }) : super(key: key);
-
-  @override
-  State<HotelList> createState() => _HotelListState(this.hotels);
-}
-
-class _HotelListState extends State<HotelList> {
-  final hotelService = HotelService();
-  List<Hotel> hotels;
-
-  _HotelListState(this.hotels);
 
   @override
   Widget build(BuildContext context) {
-    return RefreshIndicator(
-        onRefresh: () async {
-          await Future.delayed(Duration(seconds: 2));
-          updateData();
-        },
-        child: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-          ), 
-          itemCount: hotels.length,
-          itemBuilder: (context, index) {
-            return HotelStatusCard(hotel: hotels[index]);
-          }
-        ),
-        color: Colors.white,
-        backgroundColor: Colors.purple,
-        triggerMode: RefreshIndicatorTriggerMode.anywhere,
-      );
-  }
-  void updateData(){
-    hotels = hotelService.fetchHotels1();      
-    setState(() {});
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+      ), 
+      itemCount: hotels.length,
+      itemBuilder: (context, index) {
+        return HotelStatusCard(hotel: hotels[index]);
+      }
+    );
   }
 }
-
-// class HotelList extends StatelessWidget {
-//   const HotelList({ Key? key, required this.hotels }) : super(key: key);
-
-//   final List<Hotel> hotels;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return GridView.builder(
-//       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-//         crossAxisCount: 2,
-//       ), 
-//       itemCount: hotels.length,
-//       itemBuilder: (context, index) {
-//         return HotelStatusCard(hotel: hotels[index]);
-//       }
-//     );
-//   }
-// }
 
 class HotelStatusCard extends StatelessWidget {
   const HotelStatusCard({ Key? key, required this.hotel }) : super(key: key);
@@ -123,6 +129,7 @@ class HotelStatusCard extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
+
     return Card(
       color: hotel.getColor(),
       child: Column (
@@ -143,13 +150,6 @@ class HotelStatusCard extends StatelessWidget {
               ],
             ),            
           ),
-          // Padding(
-          //   padding: const EdgeInsets.all(16.0),
-          //   child: Text(
-          //     'Delayed days: ' + hotel.integration.numberOfDays.toString(),
-          //     style: TextStyle(color: Colors.black.withOpacity(0.6), fontSize: 10),              
-          //   ),
-          // )
         ],
       ),
     );
